398a8a410d6fa93da929fb394d5ab248fe8507e3
Akshay <akshay.cs18@bmsce.ac.in>
2020-09-29 20:05:19 +0530

    Added index.js boilerplae

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Home!");
+});
+
+app.post("/login", (req, res) => {
+  console.log("Log In");
+});
+
+app.post("/signup", (req, res) => {
+  console.log("Sign Up");
+});
+
+app.listen(3000, () => {
+  console.log("Working on Port 3000");
+});
