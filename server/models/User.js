d0d6ae99b4142f23095256c14f9d7358ca127255
Akshay <akshay.cs18@bmsce.ac.in>
2020-09-29 20:17:45 +0530

    Added Mongo User Schema

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// Create Schema
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  register_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Users", UserSchema);
