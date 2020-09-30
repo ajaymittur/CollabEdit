1a3b21fb86b07ab046400c3c23c5c1948d8e615f
Akshay <akshay.cs18@bmsce.ac.in>
2020-09-30 20:55:21 +0530

    Sign Up Done

--- server/index.js
+++ server/index.js
@@ -3,32 +3,89 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const authenticate = require("./middleware/authenticateToken");
-const generate = require("./models/generateToken");
+const generate = require("./controllers/generateToken");
+const User = require("./models/User");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to DB!")
+);
+mongoose.set("useCreateIndex", true);
+
 app.get("/", (req, res) => {
   console.log("Home!");
 });
 
-app.post("/login", (req, res) => {
+//Log In
+
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) return res.status(400).json("Incorrect Login");
 
-  const accessToken = generate.generateToken(req.body);
+  try {
+    const salt = bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+  } catch {
+    return res.status(500).json("Invalid Password");
+  }
 
-  res.json({ accessToken });
+  const token = generate.generateToken(req.body);
+
+  res.json({ ...req.body, token });
 
   console.log("Log In");
 });
 
-app.post("/signup", (req, res) => {
-  console.log("Sign Up");
+//Sign Up
+
+app.post("/signup", async (req, res) => {
+  const { name, username, email, dob, password } = req.body;
+
+  if (!username || !password) return res.status(400).json("Incorrect Login");
+
+  try {
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      username,
+      password: hash,
+      dob,
+    });
+
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) return res.status(540).json("User Already Exists");
+      const savedUser = await user.save();
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json("Invalid Token/Password");
+  }
+
+  const token = generate.generateToken(req.body);
+  console.log("Signed Up!");
+  const response = {
+    name,
+    email,
+    username,
+    dob,
+    token,
+  };
+  return res.json(response);
 });
 
 app.listen(process.env.PORT || 4000, () => {
