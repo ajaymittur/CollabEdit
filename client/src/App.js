e78473689714387f2c3971c6960d16ff9841978a
Ajay Mittur <ajay.cs18@bmsce.ac.in>
2020-10-04 18:48:49 +0530

    Revert "Implemented Realtime Editor Synchronization Across Different Users"

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
-
 import RichTextEditor from "./TextEditor/RichTextEditor";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to={`/groups/${uuidv4()}`} />
-        </Route>
-        <Route path="/groups/:groupId" children={<RichTextEditor />} />
-        <Route>
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <RichTextEditor />;
 }
-
-const NoMatch = () => {
-  return <h1>404</h1>;
-};
 
 export default App;
