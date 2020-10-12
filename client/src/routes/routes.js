05695d5fc498d321daa886ef5dac842bfc811c89
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-10-12 10:57:43 +0530

    Update routes

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,2 +1,5 @@
+export const ENDPOINT = "http://localhost:4000";
 export const LOGIN = "http://localhost:4000/login";
 export const SIGNUP = "http://localhost:4000/signup";
+export const GETDOCS = "http://localhost:4000/docs";
+export const GETSHAREDDOCS = "http://localhost:4000/docs/shared";
