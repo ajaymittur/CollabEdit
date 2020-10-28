b61e83dc3870b5eb86e92ec2385fe779bee1d396
Akshay <akshay.cs18@bmsce.ac.in>
2020-10-28 17:34:44 +0530

    Template for codeeditor

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import ProtectedRoute from "./auth/ProtectedRoute";
 import LoginForm from "./components/LoginForm/LoginForm";
 import SignUpForm from "./components/SignUpForm/SignUpForm";
 import NotFound from "./components/NotFound/NotFound";
 import TextEditor from "./components/TextEditor/TextEditor";
+import CodeEditor from "./components/CodeEditor/CodeEditor";
 import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
@@ -26,6 +32,9 @@ function App() {
         </ProtectedRoute>
         <Route path="/groups/:groupId">
           <TextEditor />
+        </Route>
+        <Route path="/codeEditor">
+          <CodeEditor />
         </Route>
         <Route>
           <NotFound />
