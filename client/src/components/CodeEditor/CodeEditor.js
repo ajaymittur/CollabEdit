39fc68143fdd9b7ad2725df36b12ba62a710c913
Akshay <akshay.cs18@bmsce.ac.in>
2020-10-31 21:06:07 +0530

    static syntax highlighting fone

--- client/src/components/CodeEditor/CodeEditor.js
+++ client/src/components/CodeEditor/CodeEditor.js
@@ -4,10 +4,27 @@ import "prismjs/components/prism-python";
 import "prismjs/components/prism-sql";
 import "prismjs/components/prism-java";
 import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-c";
 import React, { useState, useCallback, useMemo, useEffect } from "react";
 import { useLocation, useHistory, useParams } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 import "./prism.css";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function CodeEditor() {
   let history = useHistory();
@@ -23,15 +40,24 @@ function CodeEditor() {
 
   return (
     <>
+      <div>
+        {" "}
+        <Select
+          labelId="langSelector"
+          id="langSelect"
+          value={langname}
+          onChange={(e) => setLangname(e.target.value)}
+        >
+          <MenuItem value="javascript">Javascript</MenuItem>
+          <MenuItem value="python">Python</MenuItem>
+          <MenuItem value="c">C</MenuItem>
+          <MenuItem value="sql">SQL</MenuItem>
+          <MenuItem value="java">Java</MenuItem>
+        </Select>
+      </div>
       <PrismCode className={`language-${langname}`}>
         {`
-  onSubmit(e) {
-    e.preventDefault();
-    const job = {
-      title: 'Developer',
-      company: 'Facebook' 
-      };
-    }
+  fsdd
 `}
       </PrismCode>
     </>
