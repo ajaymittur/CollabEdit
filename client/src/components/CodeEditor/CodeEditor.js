3796cb32615244e9f7a0db693c0f46ebe5e4d112
Akshay <akshay.cs18@bmsce.ac.in>
2020-10-31 20:59:26 +0530

    Highlighting works

--- client/src/components/CodeEditor/CodeEditor.js
+++ client/src/components/CodeEditor/CodeEditor.js
@@ -1,10 +1,13 @@
 import Prism from "prismjs";
+import PrismCode from "react-prism";
 import "prismjs/components/prism-python";
-import "prismjs/components/prism-php";
 import "prismjs/components/prism-sql";
 import "prismjs/components/prism-java";
-import React, { useState, useCallback, useMemo } from "react";
+import "prismjs/components/prism-javascript";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
 import { useLocation, useHistory, useParams } from "react-router-dom";
+
+import "./prism.css";
 
 function CodeEditor() {
   let history = useHistory();
@@ -13,10 +16,26 @@ function CodeEditor() {
   const token = localStorage.getItem("token");
   const { groupId } = useParams();
   const location = useLocation();
-  //const classes = useStyles();
+  const [langname, setLangname] = useState("javascript");
   const [code, setCode] = useState("");
 
-  return <></>;
+  Prism.highlightAll();
+
+  return (
+    <>
+      <PrismCode className={`language-${langname}`}>
+        {`
+  onSubmit(e) {
+    e.preventDefault();
+    const job = {
+      title: 'Developer',
+      company: 'Facebook' 
+      };
+    }
+`}
+      </PrismCode>
+    </>
+  );
 }
 
 export default CodeEditor;
