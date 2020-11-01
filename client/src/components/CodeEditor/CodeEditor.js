e03e529d494a678dc58a2d38df789b7ac93fdc30
Akshay <akshay.cs18@bmsce.ac.in>
2020-11-01 20:41:39 +0530

    ok

--- client/src/components/CodeEditor/CodeEditor.js
+++ client/src/components/CodeEditor/CodeEditor.js
@@ -5,6 +5,9 @@ import "prismjs/components/prism-sql";
 import "prismjs/components/prism-java";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-c";
+import { Slate, Editable, withReact } from "slate-react";
+import { Text, createEditor, Node } from "slate";
+import { withHistory } from "slate-history";
 import React, { useState, useCallback, useMemo, useEffect } from "react";
 import { useLocation, useHistory, useParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
@@ -14,6 +17,15 @@ import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import {
+  EditorButton,
+  EditorLinkButton,
+  EditorPaper,
+  EditorSaveButton,
+  EditorTitle,
+  EditorToolbar,
+} from "../TextEditor/EditorComponents";
+import { css } from "emotion";
 
 import "./prism.css";
 
@@ -27,6 +39,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const initialValue = [
+  {
+    children: [
+      {
+        text: "function lol; var x = 5;",
+      },
+    ],
+  },
+];
+
 function CodeEditor() {
   let history = useHistory();
 
@@ -37,7 +59,38 @@ function CodeEditor() {
   const [langname, setLangname] = useState("javascript");
   const [code, setCode] = useState("");
 
-  Prism.highlightAll();
+  const [value, setValue] = useState(initialValue);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
+  const decorate = useCallback(
+    ([node, path]) => {
+      const ranges = [];
+      if (!Text.isText(node)) {
+        return ranges;
+      }
+      const tokens = Prism.tokenize(node.text, Prism.languages[langname]);
+      let start = 0;
+
+      for (const token of tokens) {
+        const length = getLength(token);
+        const end = start + length;
+
+        if (typeof token !== "string") {
+          ranges.push({
+            [token.type]: true,
+            anchor: { path, offset: start },
+            focus: { path, offset: end },
+          });
+        }
+
+        start = end;
+      }
+
+      return ranges;
+    },
+    [langname]
+  );
 
   return (
     <>
@@ -56,11 +109,137 @@ function CodeEditor() {
           <MenuItem value="java">Java</MenuItem>
         </Select>
       </div>
-      <PrismCode className={`language-${langname}`}>
-        {/* <TextField value={code} onChange={(e) => setCode(e.target.value)} /> */}
-      </PrismCode>
+      {/* <PrismCode className={`language-${langname}`}> */}
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(value) => setValue(value)}
+      >
+        <EditorToolbar>
+          {/* {[
+            ["bold", "format_bold"],
+            ["italic", "format_italic"],
+            ["underline", "format_underlined"],
+            ["code", "code"],
+          ].map(([format, icon]) => (
+            //<MarkButton format={format} icon={icon} disabled={readOnly} />
+            <MarkButton format={format} icon={icon} />
+          ))} */}
+          {/* <LinkButton format="link" icon="link" disabled={readOnly} /> */}
+          {/* <LinkButton format="link" icon="link" />
+          {[
+            ["heading-one", "looks_one"],
+            ["heading-two", "looks_two"],
+            ["heading-three", "looks_3"],
+            ["block-quote", "format_quote"],
+            ["numbered-list", "format_list_numbered"],
+            ["bulleted-list", "format_list_bulleted"],
+          ].map(([format, icon]) => (
+            // <BlockButton format={format} icon={icon} disabled={readOnly} />
+            <BlockButton format={format} icon={icon} />
+          ))} */}
+          <EditorTitle
+          // groupId={groupId}
+          // value={title}
+          //disabled={readOnly}
+          //handleChange={handleTitleChange}
+          />
+          <EditorSaveButton
+          // title={title}
+          // value={value}
+          //ENDPOINT={`${ENDPOINT}/docs/${groupId}`}
+          //disabled={readOnly}
+          />
+        </EditorToolbar>
+        <EditorPaper
+          //renderElement={renderElement}
+          decorate={decorate}
+          renderLeaf={renderLeaf}
+          placeholder="Start writing..."
+          spellCheck
+          autoFocus
+          // readOnly={readOnly}
+          // onKeyDown={(event) => {
+          //   for (const hotkey in HOTKEYS) {
+          //     if (isHotkey(hotkey, event)) {
+          //       event.preventDefault();
+          //       const mark = HOTKEYS[hotkey];
+          //       toggleMark(editor, mark);
+          //     }
+          //   }
+          // }}
+        />
+        {/* <Editable
+          decorate={decorate}
+          renderLeaf={renderLeaf}
+          placeholder="Write some code..."
+        /> */}
+      </Slate>
+      {/* </PrismCode> */}
     </>
   );
 }
+
+const getLength = (token) => {
+  if (typeof token === "string") {
+    return token.length;
+  } else if (typeof token.content === "string") {
+    return token.content.length;
+  } else {
+    return token.content.reduce((l, t) => l + getLength(t), 0);
+  }
+};
+
+const Leaf = ({ attributes, children, leaf }) => {
+  return (
+    <span
+      {...attributes}
+      className={css`
+        font-family: monospace;
+        background: hsla(0, 0%, 100%, 0.5);
+        ${leaf.comment &&
+        css`
+          color: slategray;
+        `}
+        ${(leaf.operator || leaf.url) &&
+        css`
+          color: #9a6e3a;
+        `}
+        ${leaf.keyword &&
+        css`
+          color: #07a;
+        `}
+        ${(leaf.variable || leaf.regex) &&
+        css`
+          color: #e90;
+        `}
+        ${(leaf.number ||
+          leaf.boolean ||
+          leaf.tag ||
+          leaf.constant ||
+          leaf.symbol ||
+          leaf.attr ||
+          leaf.selector) &&
+        css`
+          color: #905;
+        `}
+        ${leaf.punctuation &&
+        css`
+          color: #999;
+        `}
+        ${(leaf.string || leaf.char) &&
+        css`
+          color: #690;
+        `}
+        ${(leaf.function || leaf.class) &&
+        css`
+          color: #dd4a68;
+        `}
+      `}
+    >
+      {children}
+    </span>
+  );
+};
 
 export default CodeEditor;
