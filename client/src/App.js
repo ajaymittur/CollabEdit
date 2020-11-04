754758d72ccbdfca21bb4ff0bfea94635304ee16
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-11-04 21:30:20 +0530

    Update imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import ProtectedRoute from "./auth/ProtectedRoute";
 import LoginForm from "./components/LoginForm/LoginForm";
 import SignUpForm from "./components/SignUpForm/SignUpForm";
 import NotFound from "./components/NotFound/NotFound";
-import TextEditor from "./components/TextEditor/TextEditor";
-import CodeEditor from "./components/CodeEditor/CodeEditor";
+import TextEditor from "./components/Editors/TextEditor/TextEditor";
+import CodeEditorPage from "./components/Editors/CodeEditor/CodeEditorPage";
 import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
@@ -30,11 +25,11 @@ function App() {
         <ProtectedRoute path="/dashboard">
           <Dashboard />
         </ProtectedRoute>
-        <Route path="/groups/:groupId">
+        <Route path="/docs/groups/:groupId">
           <TextEditor />
         </Route>
-        <Route path="/codeEditor">
-          <CodeEditor />
+        <Route path="/code/groups/:groupId">
+          <CodeEditorPage />
         </Route>
         <Route>
           <NotFound />
