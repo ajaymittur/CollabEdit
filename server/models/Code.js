a4e8d6580eccca7eeaf35cdd6031147bbcd6780f
Akshay <akshay.cs18@bmsce.ac.in>
2020-11-05 20:17:34 +0530

    Added Code Schema

---
+++ server/models/Code.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+//Create Code Schema
+
+const CodeSchema = mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Object,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  editors: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  created_on: {
+    type: Date,
+    default: Date.now,
+  },
+  saved_on: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Code", CodeSchema);
