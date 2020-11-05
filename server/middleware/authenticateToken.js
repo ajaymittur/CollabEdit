7a2a4bd7b0405568a607a5952d8a9a635badfc2e
Akshay <akshay.cs18@bmsce.ac.in>
2020-11-05 20:15:57 +0530

    Removed console logs

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-  console.log(req.body, "before auth");
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.status(401).json("Null Token");
@@ -10,7 +9,6 @@ const authenticateToken = (req, res, next) => {
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.status(401).json("Invalid Token");
     req.body.username = decoded.username;
-    console.log(req.body, "after auth");
     next();
   });
 };
