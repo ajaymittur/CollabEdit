cf8daf6d41079d208b098f3980dd10b1d0a76e71
Akshay <akshay.cs18@bmsce.ac.in>
2020-11-06 17:10:32 +0530

    Added Editor functions

--- server/controllers/code.js
+++ server/controllers/code.js
@@ -86,13 +86,69 @@ const deleteCode = async (req, res) => {
   });
 };
 
+const addCodeEditor = async (req, res) => {
+  const { editor, username } = req.body;
+  const { groupId } = req.params;
+
+  const code = await Code.findById(groupId);
+  const user = await User.findOne({ username }, "_id");
+  const userId = user._id;
+  if (!code.owner.equals(userId))
+    return res.status(401).send(`${username} doesn't own this code`);
+
+  const _editor = await User.findOne({ username: editor }, "_id");
+  if (!_editor) return res.status(400).send(`${editor} doesn't exist`);
+  const editorId = _editor._id;
+
+  if (code.editors.indexOf(editorId) > -1)
+    return res.status(400).send(`${editor} is already an editor`);
+
+  code.editors.push(editorId);
+  await code.save();
+
+  res.status(200).send(`${editor} made an editor`);
+};
+
+const removeCodeEditor = async (req, res) => {
+  const { editor, username } = req.body;
+  const { groupId } = req.params;
+
+  const code = await Code.findById(groupId);
+  const user = await User.findOne({ username });
+  const userId = user._id;
+  if (!code.owner.equals(userId))
+    return res.status(401).send("User doesn't own this code");
+
+  const _editor = await User.findOne({ username: editor }, "_id");
+  if (!_editor) return res.status(400).send(`${editor} doesn't exist`);
+  const editorId = _editor._id;
+  if (editorId.equals(userId))
+    return res.status(400).send("Cannot remove owner from editors");
+
+  await code.updateOne({ $pull: { editors: editorId } });
+
+  res.send(`Removed ${editor}`);
+};
+
+const getCodeEditors = async (req, res) => {
+  const { groupId } = req.params;
+
+  const code = await Code.findById(groupId);
+  if (!code) return res.status(400).send("No such code exists");
+  const editors = await User.find({ _id: { $in: code.editors } }, "username");
+
+  editors.forEach((val, i) => (editors[i] = editors[i].username));
+
+  res.json(editors);
+};
+
 module.exports = {
   saveCode,
   getCode,
   getSharedCode,
   getSingleCode,
   deleteCode,
-  //   addCodeEditor,
-  //   removeCodeEditor,
-  //   getCodeEditors,
+  addCodeEditor,
+  removeCodeEditor,
+  getCodeEditors,
 };
