e26e586413500900698d49b69be5fb8ce83a1bf1
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2020-12-18 22:57:19 +0530

    FIx username not defined bug

--- client/src/components/Editors/TextEditor/TextEditor.js
+++ client/src/components/Editors/TextEditor/TextEditor.js
@@ -28,6 +28,7 @@ const useStyles = makeStyles((theme) => ({
 
 function TextEditor() {
   const name = sessionStorage.getItem("name") || "User";
+  const usernname = sessionStorage.getItem("username") || "User";
   const token = sessionStorage.getItem("token");
   const { groupId } = useParams();
   const history = useHistory();
